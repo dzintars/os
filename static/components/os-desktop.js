2549aa87ef8f7ca140daec2cdce83f92d8f9ed62
oswee <dzintars.klavins@gmail.com>
2018-06-05 13:56:06 +0300

    Random style and SignIn refactoring

--- static/components/os-desktop.js
+++ static/components/os-desktop.js
@@ -3,19 +3,9 @@ let desktop = document.createElement('template');
 desktop.innerHTML = `
     <style>
     :host {
-        display: grid;
-        grid-template-columns: repeat(auto-fill, minmax(48px, 1fr));
-        grid-template-rows: repeat(auto-fill, minmax(48px, 1fr));
         padding: 10px;
-        grid-gap: 10px;
         width: 100%;
         height: 100%;
-    }
-    :host([hbox]) {
-        flex-direction: row;
-    }
-    :host([vbox]) {
-        flex-direction: column;
     }
     </style>
     <slot></slot>
