b6779ad8e13b2aef4f3f05198f9a7791777478bb
oswee <dzintars.klavins@gmail.com>
2018-06-05 23:41:58 +0300

    Refactoring routing app navigation

--- static/js/main.js
+++ static/js/main.js
@@ -40,3 +40,14 @@ function goToDesktop() {
 function goToMain() {
     location.href = "/";
 };
+
+var mymap = L.map('mapid').setView([56.94965, 24.1052], 13);
+var marker = L.marker([56.94965, 24.1052]).addTo(mymap);
+
+L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 16,
+    id: 'mapbox.streets',
+    accessToken: 'pk.eyJ1Ijoib3N3ZWUiLCJhIjoiY2ppMXdycTZyMGY3ZDNwcXBzZnUzcTBiYiJ9.mi4BGXgJbdDgS7mVGVyExA',
+    style: 'mapbox://styles/mapbox/basic-v9',
+}).addTo(mymap);
